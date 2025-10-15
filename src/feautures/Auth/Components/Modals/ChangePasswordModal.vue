be2c7f9a6207c1385fd1b00e284823baa4f46943
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-10-15 08:04:29 -0600

    Feat:ModalPassword

---
+++ src/feautures/Auth/Components/Modals/ChangePasswordModal.vue
@@ -0,0 +1,75 @@
+<template>
+  <fwb-modal v-if="isOpen" @close="closeModal">
+    <template #header> </template>
+    <template #body>
+      <div class="w-full flex flex-col items-center gap-2">
+        <img
+          src="../../../../assets/Warning.png"
+          alt="WarningImage"
+          class="rounded-full w-1/2"
+        />
+        <span class="text-xl">Are you sure to change your password?</span>
+        <span class="text-sm font-bold"
+          >this action gonna log out yours sessions</span
+        >
+      </div>
+      <fwb-input
+        v-model="newpassword"
+        label="writte your new password"
+        placeholder="Success input"
+        required
+        validation-status="success"
+      >
+        <template #validationMessage>
+          <span class="font-medium">Well done!</span> Some success message.
+        </template>
+      </fwb-input>
+      <hr class="mt-4 border-0" />
+      <fwb-input
+        v-model="newpassword"
+        label="writte your new password"
+        placeholder="Error input"
+        required
+        validation-status="error"
+      >
+        <template #validationMessage>
+          <span class="font-medium">Oh, snapp!</span> Some error message.
+        </template>
+      </fwb-input>
+    </template>
+    <template #footer>
+      <div class="flex justify-between">
+        <fwb-button @click="closeModal" color="alternative">
+          Cancel
+        </fwb-button>
+        <fwb-button @click="handleAction" color="green"> Accept </fwb-button>
+      </div>
+    </template>
+  </fwb-modal>
+</template>
+
+<script lang="ts" setup>
+import { FwbButton, FwbModal } from "flowbite-vue";
+import { ref } from "vue";
+
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const newpassword = ref("");
+
+const emit = defineEmits<{
+  close: [];
+}>();
+
+const handleAction = () => {
+  emit("close");
+};
+
+const closeModal = () => {
+  emit("close");
+};
+</script>
---
+++ src/feautures/Home/components/Info_user_Options_Modal_Card.vue
@@ -0,0 +1,29 @@
+<template>
+  <fwb-card class="w-full bg-white shadow-gray-300 shadow-md rounded-lg">
+    <div class="w-full flex items-center gap-8 p-4 text-2xl">
+      <i :class="`pi ${props.icon} ${props.color} p-4 rounded-full`"></i>
+      <h3>{{ props.text }}</h3>
+    </div>
+  </fwb-card>
+</template>
+
+<script setup lang="ts">
+// import { FwbCard } from "flowbite-vue";
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
