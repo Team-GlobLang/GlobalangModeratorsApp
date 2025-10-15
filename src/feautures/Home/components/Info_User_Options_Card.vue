bb174af9a9967e439147a506a69a04915438f7cb
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-10-15 08:04:12 -0600

    Feat:ModalPassword

--- src/feautures/Home/components/Info_User_Options_Card.vue
+++ src/feautures/Home/components/Info_User_Options_Card.vue
@@ -1,5 +1,8 @@
 <template>
-  <fwb-card class="w-full bg-white shadow-gray-300 shadow-md rounded-lg">
+  <fwb-card
+    class="w-full bg-white shadow-gray-300 shadow-md rounded-lg"
+    @click="goto(props.route)"
+  >
     <div class="w-full flex items-center gap-8 p-4 text-2xl">
       <i :class="`pi ${props.icon} ${props.color} p-4 rounded-full`"></i>
       <h3>{{ props.text }}</h3>
@@ -8,6 +11,8 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+
 // import { FwbCard } from "flowbite-vue";
 
 const props = defineProps({
@@ -23,7 +28,17 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  route: {
+    type: String,
+    required: true,
+  },
 });
+
+const router = useRouter();
+
+function goto(pathName: string) {
+  router.push({ name: pathName });
+}
 </script>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -16,7 +16,6 @@ import Info_User from "../feautures/Home/Pages/Info_User.vue";
 import Colaborator_Request_View from "../feautures/Colaborators/pages/Colaborator_Request_View.vue";
 import Review_Quiz_Registered from "../feautures/Quiz/pages/Review_Quiz_Registered.vue";
 import Rankin_Page from "../feautures/Stats/page/Rankin_Page.vue";
-import RequestMiddlePage from "../feautures/MiddlePages/Pages/RequestMiddlePage.vue";
 const routes = [
   {
     path: "/",
@@ -58,7 +57,6 @@ const routes = [
   {
     path: "/request",
     name: "Request",
-    component: RequestMiddlePage,
     children: [
       {
         path: "quiz",
