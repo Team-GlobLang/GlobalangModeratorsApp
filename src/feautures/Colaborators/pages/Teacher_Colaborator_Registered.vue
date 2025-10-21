dd5b706fcede9aaf989fb9979f8643e7a4a9049b
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-10-21 16:02:49 -0600

    REF: Refactor collabs registereds

--- src/feautures/Colaborators/pages/Teacher_Colaborator_Registered.vue
+++ src/feautures/Colaborators/pages/Teacher_Colaborator_Registered.vue
@@ -3,24 +3,13 @@
   <section
     class="jw-full p-2 flex flex-col gap-2 justify-center items-center rounded-lg"
   >
-    <div class="sticky top-0 z-10 bg-[#f6f6f6] pb-2 w-11/12">
-      <Teacher_Colab_Registered_Filters
-        @filterchange="handleFilterChange"
-        @statusChange="handleStatus"
-      />
-    </div>
-    <Teacher_Colab_Card_Container
-      :language="languageFilter"
-      :status="statusFilter"
-    />
+    <Teacher_Colab_Card_Container />
   </section>
 </template>
 
 <script setup lang="ts">
-import Teacher_Colab_Registered_Filters from "../components/Teacher_Colab_Registered_Filters.vue";
 import Teacher_Colab_Card_Container from "../components/Teacher_Colab_Card_Container.vue";
 import BreadCrumb from "@layouts/BreadCrumb.vue";
-import { ref } from "vue";
 
 const breadCrumbItems = [
   {
@@ -30,24 +19,12 @@ const breadCrumbItems = [
   },
   {
     label: "Colaborators",
-     route: "Collaborators",
+    route: "Collaborators",
   },
   {
     label: "Registers",
   },
 ];
-
-const languageFilter = ref<string>("");
-
-const handleFilterChange = (language: string) => {
-  languageFilter.value = language;
-};
-
-const statusFilter = ref<string>("");
-
-const handleStatus = (status: string) => {
-  statusFilter.value = status;
-};
 </script>
 
 <style scoped></style>
