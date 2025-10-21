47767a82017a568e7551596b3864413d4ffcc2d5
Akion <akion.cheng.jimenez@est.una.ac.cr>
2025-10-21 12:22:13 -0600

    Ref: Refactor con filtro en Container request phrases

--- src/feautures/Phrase/components/Request_Phrase_Card_Container.vue
+++ src/feautures/Phrase/components/Request_Phrase_Card_Container.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="flex flex-col gap-4 items-center w-11/12">
-    <Phrases_Registered_Card
-      v-if="audiosRegistered.length > 0"
-      v-for="audio in audiosRegistered"
-      :key="audio.id"
-      :itemId="audio.id"
-      :admin="audio.reviewedBy || ''"
-      :meaning="audio.description"
-      :name="audio.createBy"
-      :phrase="audio.text"
-      :fileUrl="audio.fileUrl"
-      :status="audio.approved ?? false"
-      :onAction="handleAction"
-    />
+  <div class="flex flex-col items-center w-11/12">
+    <div
+      :class="stickyTopPading"
+      class="w-full flex flex-col items-center sticky z-40 bg-[#F1F4FB] pb-3"
+    >
+      <div class="w-full">
+        <FwbInput
+          list="countries"
+          v-model="country"
+          type="text"
+          :validation-status="countryError ? 'error' : undefined"
+          label="Contry"
+          placeholder="Ej: Costa Rica"
+        >
+          <template #suffix>
+            <span class="pi pi-home"></span>
+          </template>
+          <template #validationMessage>
+            <span class="font-medium">{{ countryError }} </span>
+          </template>
+        </FwbInput>
+
+        <datalist id="countries">
+          <option
+            v-for="countryItem in filteredCountries"
+            :key="countryItem.code"
+            :value="countryItem.name"
+          >
+            {{ countryItem.name }}
+          </option>
+        </datalist>
+      </div>
+
+      <div class="w-full">
+        <FwbSelect
+          v-model="selected"
+          :options="status"
+          label="Choose a status"
+          placeholder=""
+        />
+      </div>
+    </div>
+    <div class="flex flex-col w-full gap-4">
+      <Phrases_Registered_Card
+        v-if="audiosRegistered.length > 0"
+        v-for="audio in audiosRegistered"
+        :key="audio.id"
+        :itemId="audio.id"
+        :admin="audio.reviewedBy || ''"
+        :meaning="audio.description"
+        :name="audio.createBy"
+        :phrase="audio.text"
+        :fileUrl="audio.fileUrl"
+        :status="audio.approved ?? false"
+        :onAction="handleAction"
+      />
+    </div>
 
     <GoToStart v-show="showScrollTop" @click="scrollToTop" />
 
@@ -51,28 +94,42 @@
 <script setup lang="ts">
 import Phrases_Registered_Card from "./Phrases_Registered_Card.vue";
 import { useInfiniteQuery } from "@tanstack/vue-query";
-import { computed, onMounted, onUnmounted, reactive, ref, watch } from "vue";
+import { computed, onMounted, onUnmounted, reactive, ref } from "vue";
 import GoToStart from "@components/microcomponents/GoToStart.vue";
 import NotFoundVue from "@NotFound";
 import type { PaginatedResponse } from "@ComonResponse";
-import type { AudiosByFilters } from "@shared/Interfaces/AudiosByFilter";
 import { GetAllAudiosByFilters } from "@shared/Service/AudioService";
 import type { Short } from "@shared/Interfaces/Short";
 import Audio_Request_Modal from "../../Audio/components/modals/Audio_Request_Modal.vue";
+import { FwbInput } from "flowbite-vue";
+import { FwbSelect } from "flowbite-vue";
+import { useField } from "vee-validate";
+import { countries } from "@core/CountriesArray";
 
-const props = defineProps({
-  Status: {
-    type: Boolean,
-  },
-  Country: {
-    type: String,
-  },
+import { Capacitor } from "@capacitor/core";
+
+const MAX_INITIAL = 10;
+
+const filteredCountries = computed(() => {
+  if (!country.value) {
+    return countries.slice(0, MAX_INITIAL);
+  }
+  return countries.filter((c) =>
+    c.name.toLowerCase().includes(country.value.toLowerCase())
+  );
 });
 
-const filters = ref<AudiosByFilters>({
-  country: undefined,
-  approved: true,
-});
+const { value: country, errorMessage: countryError } =
+  useField<{ country: string }["country"]>("country");
+
+const selected = ref("1");
+const status = [
+  { value: "1", name: "Approved" },
+  { value: "0", name: "Rejected" },
+];
+
+const isNative = Capacitor.isNativePlatform();
+const stickyTopPading = computed(() => (isNative ? "top-[5dvh]" : "top-0"));
 
 const showScrollTop = ref(false);
 
@@ -85,11 +142,12 @@ const {
   refetch,
   isLoading,
 } = useInfiniteQuery<PaginatedResponse<Short>, Error>({
-  queryKey: computed(() => ["Request_Audios", filters]),
+  queryKey: computed(() => ["Request_Audios", country.value, selected.value]),
   queryFn: async ({ pageParam = 1 }) => {
     const page = pageParam as number;
     return await GetAllAudiosByFilters({
-      ...filters.value,
+      country: country.value || undefined,
+      approved: selected.value === "1" ? true : false,
       page,
       limit: 5,
     });
@@ -124,22 +182,6 @@ const onScroll = async () => {
 const scrollToTop = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
-
-watch(
-  () => props.Country,
-  (newCountry) => {
-    filters.value.country = newCountry;
-    refetch();
-  }
-);
-
-watch(
-  () => props.Status,
-  (newStatus) => {
-    filters.value.approved = newStatus;
-    refetch();
-  }
-);
 
 const modalState = reactive({
   isOpen: false,
--- src/feautures/Phrase/pages/Phrase_Registered.vue
+++ src/feautures/Phrase/pages/Phrase_Registered.vue
@@ -3,25 +3,13 @@
   <section
     class="w-full p-2 flex flex-col gap-2 justify-center items-center rounded-lg"
   >
-    <div class="sticky top-0 z-10 bg-[#f6f6f6] pb-2 w-11/12">
-      <Request_Phrase_Filters
-        @filterCountryChange="hadleCountryFilter"
-        @filterStatusChange="handleStatusFilter"
-      />
-    </div>
-    <Request_Phrase_Card_Container
-      :Country="CountryFilter"
-      :Status="StatusFilter"
-    />
+    <Request_Phrase_Card_Container />
   </section>
 </template>
 
 <script setup lang="ts">
-import Request_Phrase_Filters from "../components/Request_Phrase_Filters.vue";
 import Request_Phrase_Card_Container from "../components/Request_Phrase_Card_Container.vue";
-import { ref } from "vue";
 import BreadCrumb from "@layouts/BreadCrumb.vue";
-
 const breadCrumbItems = [
   {
     label: "Home",
@@ -36,16 +24,6 @@ const breadCrumbItems = [
     label: "Registers",
   },
 ];
-
-const CountryFilter = ref<string>("");
-const hadleCountryFilter = (country: string) => {
-  CountryFilter.value = country;
-};
-
-const StatusFilter = ref<boolean>(true);
-const handleStatusFilter = (status: boolean) => {
-  StatusFilter.value = status;
-};
 </script>
 
 <style scoped></style>
